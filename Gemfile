9dc85b32b00c639458c921ab0a3c5a921d7dce69
Justin Mazzi <jmazzi@gmail.com>
2013-06-05 11:54:53 -0400

    Adding app

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'mysql2'
+gem 'rspec-rails'
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecExitIssue::Application.config.secret_token = '0c8802543f435325321972582aa71eb2b80d44a95f5bf946e01d2bbd3c153a2b13f842353423928119c20676619ce8e79df58e745b467d7575f88751d668c822'
